0892eaaa97de0aef16fe6a2ee6317cb236571624
vagrant <vagrant@homestead>
2023-03-15 02:47:51 +0000

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,10 +13,10 @@
                     alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{ $user->introduction }}</p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
@@ -25,6 +25,7 @@
                 <div class="card-body">
                     <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
                 </div>
+
             </div>
             <hr>
 
