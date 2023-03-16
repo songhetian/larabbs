0f89a0d9c1bbedbff080da06fdc6b8fb2027a149
tianhesong <419097924@qq.com>
2023-03-16 16:27:59 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         //读取分类ID关联的话题 并按每20分页
-        $topics = Topic::where(['category_id' => $category->id])->paginate(20);
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->with('user', 'category')
+            ->paginate(20);
 
         //传递变量话题和分类到模板
         return view('topics.index', compact('topics', 'category'));
